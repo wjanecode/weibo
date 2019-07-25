478e5a365302656b94763b5a33f4f4fdfe3b412d
woojuan <woojuan163@163.com>
2019-07-25 09:02:10 +0800

    初始化样式

--- resources/views/static_pages/home.blade.php
+++ resources/views/static_pages/home.blade.php
@@ -2,7 +2,14 @@
 @extends('layouts.default')
 @section('title','主页')
 @section('content')
+
+<div class="jumbotron">
   <h1>WELCOME TO MY HOURSE</h1>
-  <p>this is a static page in laravel</p>
+  <p class="lead">this is a static page in laravel</p>
+  <p>all will start in here</p>
+  <p><a class="btn btn-lg btn-success" href="#" role="buttton">现在注册</a></p>
+</div>
+
+
 @stop
 
